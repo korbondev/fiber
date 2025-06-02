408c8b82b04033c1c72e011c7bfacdf87dae4516
Developer <dev@example.com>
2025-06-02 06:02:38 +0000

    frienldy message for payload decryption

--- fiber/encrypted/miner/security/encryption.py
+++ fiber/encrypted/miner/security/encryption.py
@@ -62,7 +62,7 @@ def decrypt_general_payload(
     miner_hotkey: str = Header(...),
     config: Config = Depends(get_config),
 ) -> T:
-    logger.error(f"Decrypting payload from validator {validator_hotkey} for miner {miner_hotkey}")
+    logger.info(f"Decrypting payload from validator {validator_hotkey} for miner {miner_hotkey}")
     symmetric_key_info = config.encryption_keys_handler.get_symmetric_key(validator_hotkey, symmetric_key_uuid)
     if not symmetric_key_info:
         raise HTTPException(status_code=429, detail="No symmetric key found for that hotkey and uuid")
